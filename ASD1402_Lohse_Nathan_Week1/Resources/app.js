88629c566e0dbec4900dbd486726c0a6f82570bd
b81984 <nlohse@fullsail.edu>
2014-02-06 20:01:39 -0600

    First 5 objects built in literal notation

--- ASD1402_Lohse_Nathan_Week1/Resources/app.js
+++ ASD1402_Lohse_Nathan_Week1/Resources/app.js
@@ -1,64 +1,154 @@
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
-Titanium.UI.setBackgroundColor('#000');
-
-// create tab group
-var tabGroup = Titanium.UI.createTabGroup();
-
-
-//
-// create base UI tab and root window
-//
-var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
-    backgroundColor:'#fff'
-});
-var tab1 = Titanium.UI.createTab({  
-    icon:'KS_nav_views.png',
-    title:'Tab 1',
-    window:win1
+var mainWindow = Ti.UI.createWindow({
+	modal: true,
+	title: "Objects"
 });
 
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 1',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
+var navWindow = Ti.UI.createWindow({
+	title: "Objects",
+	modal: true,
+	backgroundColor: "#FFF"
 });
 
-win1.add(label1);
-
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
+var navGroup = Ti.UI.iOS.createNavigationWindow({
+	window: navWindow
 });
-var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
+var scrollView = Ti.UI.createScrollView({
+	layout: "vertical"
+});
+var object0 = Ti.UI.createView({
+	height: 50,
+	top: 5,
+	left: 5,
+	right: 5,
+	backgroundColor: "#606060",
+	layout: "vertical"
+});
+var object1 = Ti.UI.createView({
+	height: 50,
+	top: 5,
+	left: 5,
+	right: 5,
+	backgroundColor: "#606060",
+	layout: "vertical"
+});
+var object2 = Ti.UI.createView({
+	height: 50,
+	top: 5,
+	left: 5,
+	right: 5,
+	backgroundColor: "#606060",
+	layout: "vertical"
+});
+var object3 = Ti.UI.createView({
+	height: 70,
+	top: 5,
+	left: 5,
+	right: 5,
+	backgroundColor: "#606060",
+	layout: "vertical"
+});
+var object4 = Ti.UI.createView({
+	height: 70,
+	top: 5,
+	left: 5,
+	right: 5,
+	backgroundColor: "#606060",
+	layout: "vertical"
+});
+var object5 = Ti.UI.createView({
+	height: 50,
+	top: 5,
+	left: 5,
+	right: 5,
+	backgroundColor: "#606060",
+	layout: "vertical"
+});
+var object6 = Ti.UI.createView({
+	height: 50,
+	top: 5,
+	left: 5,
+	right: 5,
+	backgroundColor: "#606060",
+	layout: "vertical"
+});
+var object7 = Ti.UI.createView({
+	height: 50,
+	top: 5,
+	left: 5,
+	right: 5,
+	backgroundColor: "#606060",
+	layout: "vertical"
+});
+var object8 = Ti.UI.createView({
+	height: 50,
+	top: 5,
+	left: 5,
+	right: 5,
+	backgroundColor: "#606060",
+	layout: "vertical"
+});
+var object9 = Ti.UI.createView({
+	height: 50,
+	top: 5,
+	left: 5,
+	right: 5,
+	backgroundColor: "#606060",
+	layout: "vertical"
 });
 
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
+var label0 = Ti.UI.createLabel({
+	
 });
+var label1 = Ti.UI.createLabel({
+	
+});
+var label2 = Ti.UI.createLabel({
+	
+});
+var label3 = Ti.UI.createLabel({
+	
+});
+var label4 = Ti.UI.createLabel({
+	
+});
+var label5 = Ti.UI.createLabel({
+	
+});
+var label6 = Ti.UI.createLabel({
+	
+});
+var label7 = Ti.UI.createLabel({
+	
+});
+var label8 = Ti.UI.createLabel({
+	
+});
+var label9 = Ti.UI.createLabel({
+	
+});
+var label10 = Ti.UI.createLabel({
+	
+});
+var label11 = Ti.UI.createLabel({
+	
+});
+object0.add(label0);
+object0.add(label1);
+object1.add(label2);
+object1.add(label3);
+object2.add(label4);
+object2.add(label5);
+object3.add(label6);
+object3.add(label7);
+object4.add(label8);
+object4.add(label9);
+scrollView.add(object0);
+scrollView.add(object1);
+scrollView.add(object2);
+scrollView.add(object3);
+scrollView.add(object4);
+var objectDemoFile = require('objectDemo');
 
-win2.add(label2);
-
-
-
-//
-//  add tabs
-//
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
-
-
-// open tab group
-tabGroup.open();
+navWindow.add(scrollView);
+mainWindow.add(navGroup);
+navGroup.open();
